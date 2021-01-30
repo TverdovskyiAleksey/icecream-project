150f1da1e0a31063ebbfcc0cf71b2e71bd1ee436
Alex <tverdovskyi92@gmail.com>
2021-01-30 15:27:11 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
+},{"./..\\images\\comments\\img2-mobile@1x.svg":[["img2-mobile@1x.3dc48dc6.svg","images/comments/img2-mobile@1x.svg"],"images/comments/img2-mobile@1x.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 require("./sass/main.scss");
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58163" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64667" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
